c5e47ea0645800042b8ee237ef43b50ac0c883f9
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-04-21 19:19:49 +0700

    add_basket_model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -19,3 +20,12 @@ class Product(models.Model):
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
 
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)  #Привязываемся к таблице User
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)  #Привязываемся к таблице Product
+    quantity = models.PositiveSmallIntegerField(default=0)  #Добавляем колличество товаров в корзине
+    created_timestamp = models.DateTimeField(auto_now_add=True)  # Добавляем время добавления в корзину
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
