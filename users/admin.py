6afe69e7ba04f653e4a655ff148ad355d0350433
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-04-24 22:10:50 +0700

    update_admin_user

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from users.models import User
+from products.admin import BasketAdmin
 
-admin.site.register(User)  # Регистрируем модель в админке
+ # Регистрируем модель в админке
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username',)
+    inlines = (BasketAdmin,)
