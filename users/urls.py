ee1dea7f3c4f03dde0f7c28beda57c499c7cccf7
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-05-01 16:00:54 +0700

    update_useer_url

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
+from django.urls import path
 
-from users.views import UserLoginView, UserRegistrationView, UserProfileView
+from users.views import (EmailVerificationView, UserLoginView, UserProfileView,
+                         UserRegistrationView)
 
 app_name = 'users'
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>', login_required(UserProfileView.as_view()), name='profile'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name='email_verification'),
 ]
