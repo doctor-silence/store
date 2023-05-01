b0ba9d23766f60587bd74e2d9636e0a59acc1a11
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-05-01 16:00:09 +0700

    update_user_model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,41 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
+from django.db import models
+from django.urls import reverse
+from django.utils.timezone import now
 
 # Добавляем новые поля в модель Юзер
+
 class User(AbstractUser):
     image = models.ImageField(upload_to='user_images', null=True, blank=True)
+    is_verified_email = models.BooleanField(default=False)   # Подтверждение email
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)   # Формируем универсальный Идентификатор
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)   # Автоматическое заполнение при создани объекта
+    expiration = models.DateTimeField()      # Добавления срока жизни ссылки
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'
+
+    def send_verification_email(self):
+        link = reverse('users:email_verification', kwargs={'email': self.user.email, 'code': self.code})
+        verification_link = f'{settings.DOMAIN_NAME}{link}'
+        subject = f'Подтверждение учетной записи {self.user.username}'
+        message = 'Для подтверждения учетной записи {} перейдите по ссылке: {}'.format(
+            self.user.email,
+            verification_link
+        )
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[self.user.email],
+            fail_silently=False,
+        )
+
+    def is_expired(self):
+        return True if now() >= self.expiration else False
