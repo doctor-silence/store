dafd4e75613882ab418f5ac67ebe0d17083d50b7
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-05-01 15:59:36 +0700

    update_user_admin

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from users.models import User
-from products.admin import BasketAdmin
 
- # Регистрируем модель в админке
+from products.admin import BasketAdmin
+from users.models import EmailVerification, User
+
+# Регистрируем модель в админке
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username',)
     inlines = (BasketAdmin,)
+
+
+@admin.register(EmailVerification)
+class EmailVerification(admin.ModelAdmin):
+    list_display = ('code', 'user', 'expiration')
+    field = ('code', 'user', 'expiration', 'created')
+    readonly_fields = ('created',)
